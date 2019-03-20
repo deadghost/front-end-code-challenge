f58b6145dc9224cb57cd7b182bb7e0c1a430b828
Kenny Liu <kennyoliu@gmail.com>
2019-03-20 04:13:18 -0700

    Scrub cljs related things.

--- project.clj
+++ project.clj
@@ -9,16 +9,10 @@
                  [io.pedestal/pedestal.immutant "0.5.4"]
                  [ring/ring-defaults "0.2.1"] ; Middleware
                  [bidi "2.1.2"] ; Routing
-
-                 ;; Clojurescript Dependencies
-                 [org.clojure/clojurescript "1.9.908"]]
+                 ]
   :source-paths ["src/clj" "src/cljc"]
   :target-path "target/%s"
-  :plugins [[lein-garden "0.3.0"]
-            [lein-cljsbuild "1.1.7"]
-            [lein-figwheel "0.5.13"]]
-  :figwheel {:css-dirs ["resources/public/css"]
-             :server-port 3549}
+  :plugins [[lein-garden "0.3.0"]]
   :garden {:builds [{:id "app"
                      :source-paths ["src/clj"]
                      :stylesheet roomkey.site.css.app/app-stylesheet
@@ -27,26 +21,5 @@
                                 :pretty-print? false}}]}
   :profiles
   {:uberjar {:aot :all}
-   :dev {:cljsbuild
-         {:builds
-          [{:id "app"
-            :source-paths ["src/cljs" "src/cljc"]
-            :compiler {:output-to "resources/public/js/app.js"
-                       :output-dir "resources/public/js/out"
-                       :asset-path "/js/out"
-                       :main "roomkey.app"
-                       :pretty-print false
-                       :source-map true
-                       :optimizations :none}
-            :notify-command ["notify-send"]
-            :figwheel true}]}}
-   :prod {:cljsbuild
-          {:builds
-           [{:id "app"
-             :source-paths ["src/cljs" "src/cljc"]
-             :compiler {:output-to "resources/public/js/app.js"
-                        :output-dir "resources/public/js/out"
-                        :asset-path "/js/out"
-                        :main "roomkey.app"
-                        :pretty-print false
-                        :optimizations :advanced}}]}}})
+   :dev {}
+   :prod {}})
